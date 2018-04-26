ccb9b096e0f774447ef9fa908ac8e1dd1dd7f8f7
Raphaël Londeix <raphael.londeix@celoxica.com>
2018-04-26 13:35:29 +0100

    remove tabs
    Change-Id: Ic83d11d9e8f0f4219d2d48ed8b4045c98fc6b755

--- build.sh
+++ build.sh
@@ -14,116 +14,116 @@ ${CMAKE} --version
 
 ############################# build neovim
 if [ ! -f "${INSTALL_DIR}"/bin/nvim ]; then
-	echo "Building Neovim"
+    echo "Building Neovim"
 
-	cd "${SCRIPT_DIR}"/neovim
-	mkdir -p .deps
-	cd .deps
-	${CMAKE} ../third-party -DCMAKE_BUILD_TYPE=Release > "${SCRIPT_DIR}"/neovim.log
-	make >> "${SCRIPT_DIR}"/neovim.log
+    cd "${SCRIPT_DIR}"/neovim
+    mkdir -p .deps
+    cd .deps
+    ${CMAKE} ../third-party -DCMAKE_BUILD_TYPE=Release > "${SCRIPT_DIR}"/neovim.log
+    make >> "${SCRIPT_DIR}"/neovim.log
 
-	cd "${SCRIPT_DIR}"/neovim
-	mkdir -p build
-	cd build
-	${CMAKE} .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" >> "${SCRIPT_DIR}"/neovim.log
-	make -j${CORES} >> "${SCRIPT_DIR}"/neovim.log
-	make install >> "${SCRIPT_DIR}"/neovim.log
-	mv "${INSTALL_DIR}/bin/nvim" "${INSTALL_DIR}/bin/nvim-binary"
-	cp "${SCRIPT_DIR}/launcher.sh" "${INSTALL_DIR}/bin/nvim"
+    cd "${SCRIPT_DIR}"/neovim
+    mkdir -p build
+    cd build
+    ${CMAKE} .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" >> "${SCRIPT_DIR}"/neovim.log
+    make -j${CORES} >> "${SCRIPT_DIR}"/neovim.log
+    make install >> "${SCRIPT_DIR}"/neovim.log
+    mv "${INSTALL_DIR}/bin/nvim" "${INSTALL_DIR}/bin/nvim-binary"
+    cp "${SCRIPT_DIR}/launcher.sh" "${INSTALL_DIR}/bin/nvim"
 fi
 
 ############################# build xsel
 if [ ! -f "${INSTALL_DIR}"/bin/xsel ]; then
-	echo "Building xsel"
-	cd "${SCRIPT_DIR}"/xsel*
-	mkdir -p build
-	cd build
-	../configure --prefix="${INSTALL_DIR}"
-	make
-	make install
+    echo "Building xsel"
+    cd "${SCRIPT_DIR}"/xsel*
+    mkdir -p build
+    cd build
+    ../configure --prefix="${INSTALL_DIR}"
+    make
+    make install
 fi
 
 ############################ build zlib
 if [ ! -f "${INSTALL_DIR}"/lib/libz.so ]; then
-	echo "Building zlib"
-	cd "${SCRIPT_DIR}"/zlib*
-	mkdir -p build
-	cd build
-	${CMAKE} .. -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" > "${SCRIPT_DIR}"/zlib.log
-	make install >> "${SCRIPT_DIR}"/zlib.log
+    echo "Building zlib"
+    cd "${SCRIPT_DIR}"/zlib*
+    mkdir -p build
+    cd build
+    ${CMAKE} .. -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" > "${SCRIPT_DIR}"/zlib.log
+    make install >> "${SCRIPT_DIR}"/zlib.log
 fi
 
 ############################ build openssl
 if [ ! -f "${INSTALL_DIR}"/lib/libssl.a ]; then
-	echo "Building OpenSSL"
-	cd "${SCRIPT_DIR}"/openssl
-	./Configure linux-x86_64 no-shared \
-		-fPIC \
-		-I"${INSTALL_DIR}"/include \
-		-L"${INSTALL_DIR}"/lib \
-		--prefix="${INSTALL_DIR}" \
-		--openssldir="${INSTALL_DIR}/ssl" > "${SCRIPT_DIR}"/openssl.log
-	make >> "${SCRIPT_DIR}"/openssl.log
-	make install >> "${SCRIPT_DIR}"/openssl.log
+    echo "Building OpenSSL"
+    cd "${SCRIPT_DIR}"/openssl
+    ./Configure linux-x86_64 no-shared \
+        -fPIC \
+        -I"${INSTALL_DIR}"/include \
+        -L"${INSTALL_DIR}"/lib \
+        --prefix="${INSTALL_DIR}" \
+        --openssldir="${INSTALL_DIR}/ssl" > "${SCRIPT_DIR}"/openssl.log
+    make >> "${SCRIPT_DIR}"/openssl.log
+    make install >> "${SCRIPT_DIR}"/openssl.log
 fi
 
 ############################ build python3
 if [ ! -f "${INSTALL_DIR}"/bin/python3 ]; then
-	echo "Building Python 3"
-	cd "${SCRIPT_DIR}"/Python-3.*
-	mkdir -p build
-	cd build
-	../configure \
-		--prefix="${INSTALL_DIR}" \
-		--enable-shared \
-		LDFLAGS="-Wl,-rpath=${INSTALL_DIR}/lib" > "${SCRIPT_DIR}"/python-3.log
-	make -j${CORES} >> "${SCRIPT_DIR}"/python-3.log
-	make install >> "${SCRIPT_DIR}"/python-3.log
-	"${INSTALL_DIR}"/bin/pip3 install neovim >> "${SCRIPT_DIR}"/python-3.log
-	"${INSTALL_DIR}"/bin/pip3 install python-language-server >> "${SCRIPT_DIR}"/python-3.log
+    echo "Building Python 3"
+    cd "${SCRIPT_DIR}"/Python-3.*
+    mkdir -p build
+    cd build
+    ../configure \
+        --prefix="${INSTALL_DIR}" \
+        --enable-shared \
+        LDFLAGS="-Wl,-rpath=${INSTALL_DIR}/lib" > "${SCRIPT_DIR}"/python-3.log
+    make -j${CORES} >> "${SCRIPT_DIR}"/python-3.log
+    make install >> "${SCRIPT_DIR}"/python-3.log
+    "${INSTALL_DIR}"/bin/pip3 install neovim >> "${SCRIPT_DIR}"/python-3.log
+    "${INSTALL_DIR}"/bin/pip3 install python-language-server >> "${SCRIPT_DIR}"/python-3.log
 fi
 
 ############################# build python2
 if [ ! -f "${INSTALL_DIR}"/bin/python2 ]; then
-	echo "Building Python 2"
-	cd "${SCRIPT_DIR}"/Python-2.*
-	mkdir -p build
-	cd build
-	../configure \
-		--prefix="${INSTALL_DIR}" \
-		--enable-unicode=ucs4 \
-		--enable-shared \
-		--with-ensurepip=install \
-		LDFLAGS="-Wl,-rpath=${INSTALL_DIR}/lib" > "${SCRIPT_DIR}"/python-2.log
-	make -j${CORES} >> "${SCRIPT_DIR}"/python-2.log
-	make install >> "${SCRIPT_DIR}"/python-2.log
-	"${INSTALL_DIR}"/bin/pip2.7 install neovim >> "${SCRIPT_DIR}"/python-2.log
-	"${INSTALL_DIR}"/bin/pip2.7 install python-language-server >> "${SCRIPT_DIR}"/python-2.log
+    echo "Building Python 2"
+    cd "${SCRIPT_DIR}"/Python-2.*
+    mkdir -p build
+    cd build
+    ../configure \
+        --prefix="${INSTALL_DIR}" \
+        --enable-unicode=ucs4 \
+        --enable-shared \
+        --with-ensurepip=install \
+        LDFLAGS="-Wl,-rpath=${INSTALL_DIR}/lib" > "${SCRIPT_DIR}"/python-2.log
+    make -j${CORES} >> "${SCRIPT_DIR}"/python-2.log
+    make install >> "${SCRIPT_DIR}"/python-2.log
+    "${INSTALL_DIR}"/bin/pip2.7 install neovim >> "${SCRIPT_DIR}"/python-2.log
+    "${INSTALL_DIR}"/bin/pip2.7 install python-language-server >> "${SCRIPT_DIR}"/python-2.log
 fi
 
 export PATH="${INSTALL_DIR}/bin":$PATH
 
 ############################# build LLVM and Clang
 if [ ! -f "${INSTALL_DIR}"/lib/libclang.so ]; then
-	echo "Building LLVM and clang"
-	cd "${SCRIPT_DIR}"/llvm*
-	mkdir -p build
-	cd build
-	${CMAKE} .. \
-		-DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" \
-		-DCMAKE_BUILD_TYPE=Release \
-		-DPYTHON_EXECUTABLE="${INSTALL_DIR}/bin/python2.7" > "${SCRIPT_DIR}"/llvm.log
-	make -j${CORES} >> "${SCRIPT_DIR}"/llvm.log
-	make install >> "${SCRIPT_DIR}"/llvm.log
-	cd "${INSTALL_DIR}"
-	cp share/clang/clang-format.py bin
+    echo "Building LLVM and clang"
+    cd "${SCRIPT_DIR}"/llvm*
+    mkdir -p build
+    cd build
+    ${CMAKE} .. \
+        -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DPYTHON_EXECUTABLE="${INSTALL_DIR}/bin/python2.7" > "${SCRIPT_DIR}"/llvm.log
+    make -j${CORES} >> "${SCRIPT_DIR}"/llvm.log
+    make install >> "${SCRIPT_DIR}"/llvm.log
+    cd "${INSTALL_DIR}"
+    cp share/clang/clang-format.py bin
 fi
 
 
 ############################# build cquery
 if [ ! -f "${INSTALL_DIR}"/bin/cquery ]; then
-	echo "Building cquery"
-	cd "${SCRIPT_DIR}"/cquery
+    echo "Building cquery"
+    cd "${SCRIPT_DIR}"/cquery
     ./waf configure \
         --llvm-config="${INSTALL_DIR}/bin/llvm-config" \
         --variant=release \
